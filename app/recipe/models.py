bb5a70fdd9dfb686cd7f945a34a4ed50a31abf45
Joe Buckley <joe.buckley@travelperk.com>
2022-02-07 16:27:36 +0100

    refactor Recipe model description field to be TextField

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
 
 
 class Ingredient(models.Model):
